15dcfd7ae59c563fddc261bcf2a816c9ba0148f3
Amer Tahri Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2025-02-18 23:51:26 +0100

    Update sum.c

--- sum.c
+++ sum.c
@@ -3,17 +3,11 @@
 int main() {
     int num1, num2, sum;
 
-    // Get user input
     printf("Enter first number: ");
     scanf("%d", &num1);
-
     printf("Enter second number: ");
     scanf("%d", &num2);
-
-    // Calculate sum
     sum = num1 + num2;
-
-    // Display result
     printf("Sum: %d\n", sum);
 
     return 0;
